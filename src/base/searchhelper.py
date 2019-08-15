809d5ab79cfea355f8b7a93e930521779e23ca5b
N Copiy <kopiy@group-ib.ru>
2019-08-15 19:40:20 +0300

    Add docs to search helper

--- src/base/searchhelper.py
+++ src/base/searchhelper.py
@@ -1,11 +1,22 @@
 class SearchHelperMixin:
+    """
+    SearchHelperMixin provides possibility to get mapping and search helpers for user-friendly search API
+    """
+
     fields_to_exclude_from_mapping = None
 
-    _mapping = None
+    _full_mapping = None
     _raw_mapping = None
 
     @classmethod
     def exclude_mapping_fields(cls, mapping):
+        """
+        Returns mapping without fields to exclude
+        it may be:
+            1) sources inside of fields
+            2) fields in cls.fields_to_exclude_from_mapping
+        """
+
         if cls.fields_to_exclude_from_mapping is not None:
             fields_to_exclude_from_mapping = cls.fields_to_exclude_from_mapping
         else:
@@ -19,18 +30,28 @@ class SearchHelperMixin:
 
     @classmethod
     def get_mapping(cls):
-        if cls._mapping is None:
-            cls._mapping = sorted(cls.exclude_mapping_fields(cls._get_mapping()))
-        return cls._mapping
+        """
+        Returns full mapping with handwritten fields in search set class and their sources
+        Except of excluded fields/sources
+        """
+        if cls._full_mapping is None:
+            cls._full_mapping = sorted(cls.exclude_mapping_fields(cls._get_mapping()))
+        return cls._full_mapping
 
     @classmethod
     def get_raw_mapping(cls):
+        """
+        Caches raw mapping and return it
+        """
         if cls._raw_mapping is None:
             cls._raw_mapping = cls._get_raw_mapping()
         return cls._raw_mapping
 
     @classmethod
     def _get_mapping(cls):
+        """
+        Returns mapping extended by handwritten fields and its sources
+        """
         all_sources = []
 
         for field_name, field in cls.get_field_name_to_field().items():
@@ -43,4 +64,7 @@ class SearchHelperMixin:
 
     @classmethod
     def _get_raw_mapping(cls):
+        """
+        That method allows to get mapping in raw format. It have to be reimplemented
+        """
         raise NotImplementedError()
