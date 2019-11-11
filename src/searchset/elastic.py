9626c82a3a90c22925b28160904662e7caa3e50e
N Copiy <kopiy@group-ib.ru>
2019-11-11 16:01:52 +0300

    Fix typo

--- src/searchset/elastic.py
+++ src/searchset/elastic.py
@@ -6,7 +6,7 @@ from ..searchset.utils import FieldType
 from ..parser import LuceneToElasticParserMixin
 
 from .base import BaseSearchSet
-from .fields.elastic import default_eclipse_field_types_to_fields
+from .fields.elastic import default_elastic_field_types_to_fields
 
 
 elastic_data_type_to_field_type = {
@@ -23,7 +23,7 @@ class ElasticSearchSet(LuceneToElasticParserMixin, BaseSearchSet):
     _field_base_class = ElasticSearchField
     _default_field = ElasticSearchField
 
-    _field_type_to_field_class = default_eclipse_field_types_to_fields
+    _field_type_to_field_class = default_elastic_field_types_to_fields
     _elastic_data_type_to_field_type = elastic_data_type_to_field_type
 
     @classmethod
