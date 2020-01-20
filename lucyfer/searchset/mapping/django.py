f188044169869992a1da912ef864a832f6e081aa
N Copiy <kopiy@group-ib.ru>
2020-01-20 17:05:07 +0300

    Do not use filters if empty prefix

--- lucyfer/searchset/mapping/django.py
+++ lucyfer/searchset/mapping/django.py
@@ -5,18 +5,18 @@ from lucyfer.searchset.mapping.base import MappingValue, Mapping
 
 class DjangoMappingValue(MappingValue):
     def _get_values(self, qs, prefix: str) -> List[str]:
-        result_qs = qs.filter(**{'{}__icontains'.format(self.sources[0]): prefix}).\
-            values_list(self.sources[0], flat=True).distinct()
+        if prefix:
+            qss = [
+                qs.filter(**{'{}__icontains'.format(source): prefix}).values_list(source, flat=True).distinct()
+                for source in self.sources
+            ]
+        else:
+            qss = [
+                qs.values_list(source, flat=True).distinct()
+                for source in self.sources
+            ]
 
-        other_qss = [
-            qs.filter(**{'{}__icontains'.format(source): prefix}).values_list(source, flat=True).distinct()
-            for source in self.sources[1:]
-        ]
-
-        if other_qss:
-            result_qs = result_qs.union(*other_qss)
-
-        return list(set(result_qs))
+        return list(set(qss.pop().union(*qss)))
 
 
 class DjangoMapping(Mapping):
