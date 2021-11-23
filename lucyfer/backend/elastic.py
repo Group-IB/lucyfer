5fc206f88fe722abfd8deef080608334d056e95c
Alexey Mokeev <olerdrive@gmail.com>
2021-11-23 13:48:06 +0300

    Update elastic.py

--- lucyfer/backend/elastic.py
+++ lucyfer/backend/elastic.py
@@ -7,10 +7,6 @@ class ElasticLuceneSearchFilterMixin(BaseEsFilterBackend):
     def filter_search(self, request, search, view):
         search_terms = self.get_base_search_terms(request)
 
-        # if there nothing to search - return
-        if not search_terms:
-            return search
-
         searchset_class = self.get_searchset_class(view, request)
         filtered_search = self.lucene_filter_search(searchset_class=searchset_class, search_terms=search_terms,
                                                     search=search)
